13ebbeb9b294ef8f446bc60cb2a1409ef123059c
Elizeus29 <eliseo.cv98@gmail.com>
2021-04-24 14:40:52 -0400

    listo

--- api.php
+++ api.php
@@ -17,8 +17,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
     if (isset($_GET['patente']))
     {
       //Mostrar un post
-      $sql = $dbConn->prepare("SELECT * FROM vehiculo where patente=:patente");
-      $sql->bindValue(':patente', $_GET['patente']);
+      $sql = $dbConn->prepare("SELECT * FROM imagenesLGBT where id=:id");
+      $sql->bindValue(':id', $_GET['id']);
       $sql->execute();
       header("HTTP/1.1 200 OK");
       echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
 	  }
     else {
       //Mostrar lista de post
-      $sql = $dbConn->prepare("SELECT * FROM vehiculo");
+      $sql = $dbConn->prepare("SELECT * FROM imagenesLGBT");
       $sql->execute();
       $sql->setFetchMode(PDO::FETCH_ASSOC);
       header("HTTP/1.1 200 OK");
@@ -35,61 +35,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
 	}
 }
 
-// Crear un nuevo post
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-		
-    $input = $_GET;
-    echo "".$input;
-    $sql = "INSERT INTO vehiculo
-          (patente, color, anio, precio, kilometros, id_marca, id_modelo)
-          VALUES
-          (:patente, :color, :anio, :precio, :kilometros, :id_marca, :id_modelo)";
-    $statement = $dbConn->prepare($sql);
-    bindAllValues($statement, $input);
-    $statement->execute();
-    $postId = $dbConn->lastInsertId();
-    if($postId)
-    {
-      $input['patente'] = $postId;
-      header("HTTP/1.1 200 OK");
-      echo json_encode($input);
-      echo "insertado";
-      exit();
-	 }
-}
-
-//Borrar
-if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
-{
-	$id = $_GET['patente'];
-  $statement = $dbConn->prepare("DELETE FROM vehiculo where patente=:patente");
-  $statement->bindValue(':patente', $id);
-  $statement->execute();
-	header("HTTP/1.1 200 OK");
-	exit();
-}
-
-//Actualizar
-if ($_SERVER['REQUEST_METHOD'] == 'PUT')
-{
-    $input = $_GET;
-    $postId = $input['patente'];
-    $fields = getParams($input);
-
-    $sql = "
-          UPDATE vehiculo
-          SET $fields
-          WHERE patente='$postId'
-           ";
-
-    $statement = $dbConn->prepare($sql);
-    bindAllValues($statement, $input);
-
-    $statement->execute();
-    header("HTTP/1.1 200 OK");
-    exit();
-}
 
 
 //En caso de que ninguna de las opciones anteriores se haya ejecutado
